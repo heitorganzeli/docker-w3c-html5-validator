b244c1cdef132155b41db7cacae6e355e80b3b56
Heitor Ganzeli <heitor@nic.br>
2016-12-28 13:55:49 -0200

    fix reboot sh

--- reboot_validator.sh
+++ reboot_validator.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 /bin/systemctl stop docker-w3c_validator.service
-/usr/bin/dockerdocker rm w3c_validator
-/usr/bin/dockerdocker docker run -d -p 45000:80 --name w3c_validator nicbr/w3c-html5-validator
+/usr/bin/docker rm w3c_validator
+/usr/bin/docker docker run -d -p 45000:80 --name w3c_validator nicbr/w3c-html5-validator
+/bin/systemctl start docker-w3c_validator.service
