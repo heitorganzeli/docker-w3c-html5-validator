4ddbe0137bbe3636e15792fdba22fab03120d8de
Heitor Ganzeli <heitor@nic.br>
2017-02-02 08:55:45 -0200

    add image re build to reboot process

--- reboot_validator.sh
+++ reboot_validator.sh
@@ -2,5 +2,6 @@
 
 /bin/systemctl stop docker-w3c_validator.service
 /usr/bin/docker rm w3c_validator
+/usr/bin/docker build -t nicbr/w3c-html5-validator .
 /usr/bin/docker run -d -p 45000:80 --name w3c_validator nicbr/w3c-html5-validator
 /bin/systemctl start docker-w3c_validator.service
